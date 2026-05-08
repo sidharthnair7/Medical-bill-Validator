c5c67671d03f668aa83bd76cf454cf8545b3fcba
Sidharth Nair <realsid6@gmail.com>
2026-05-08 18:46:45 -0400

    Initial Push

---
+++ src/test/java/com/example/medbillhackathon/MedbillHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medbillhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedbillHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
